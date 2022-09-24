923a5a36d75396dc6e883920bc7ff2224aef1eda
malory.beguem <euphorya@outlook.jp>
2022-09-24 15:28:18 +0200

    add port to server

--- back/server.js
+++ back/server.js
@@ -1,2 +1,7 @@
 const http = require('http');
 const app = require('./app');
+
+pp.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000);
