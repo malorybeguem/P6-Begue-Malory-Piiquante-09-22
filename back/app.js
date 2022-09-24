8c7c450bd9cb6e639a30688f7e4104b83a516cbf
malory.beguem <euphorya@outlook.jp>
2022-09-24 15:31:25 +0200

    add of sauce and user route js

--- back/app.js
+++ back/app.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
