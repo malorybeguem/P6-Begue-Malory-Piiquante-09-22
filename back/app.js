9827746d638a460a44d404b3e67317978229ec30
malory.beguem <euphorya@outlook.jp>
2022-09-24 15:49:14 +0200

    some changes in app

--- back/app.js
+++ back/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const app = express();
+
 const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
+
+module.exports = app;
