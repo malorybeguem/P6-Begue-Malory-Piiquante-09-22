0e4e38d5fb3e5c7eafd7a5df20590d1bf76250b7
malory.beguem <euphorya@outlook.jp>
2022-09-25 18:12:43 +0200

    add const on app js file

--- back/app.js
+++ back/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const app = express();
-
 const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
+const helmet = require('helmet');
+const path = require('path');
+require('dotenv').config({path: '.env'});
+
+
+const app = express();
 
 module.exports = app;
