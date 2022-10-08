07b5616bf989fb9058a2c4091548cc8f350a627b
malory.beguem <euphorya@outlook.jp>
2022-10-08 22:41:22 +0200

    route sauce creation

---
+++ back/routes/sauce.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middlewares/auth');
+const multer = require('../middlewares/multer-config');
+
+const sauceCtrl = require('../controllers/sauce');
+
+router.get('/', auth, sauceCtrl.getSauces);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.post("/:id/like", auth, sauceCtrl.likeSauce);
+
+
+module.exports = router;
